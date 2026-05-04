adc6967c0417b0dc509b97c2167a4048f15dcdac
JayeshWani7 <wanijayesh0@gmail.com>
2026-05-04 17:32:05 +0530

    feat: bind server to all network interfaces for accessibility

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ mongoose
     tlsAllowInvalidCertificates: false,
   })
   .then(() => {
-    app.listen(PORT, () => {
+      app.listen(PORT, '0.0.0.0' , () => {
       console.log(`Server listening on port ${PORT}`);
     });
   })
